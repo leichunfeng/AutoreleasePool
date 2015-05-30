84145cd1278f4ca7024e5fe58ebe006be10e3ffe
leichunfeng <307213080@qq.com>
2015-05-30 10:23:10 +0800

    Create project.

---
+++ AutoreleasePool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoreleasePool
+//
+//  Created by leichunfeng on 15/5/30.
+//  Copyright (c) 2015年 leichunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoreleasePool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoreleasePool
+//
+//  Created by leichunfeng on 15/5/30.
+//  Copyright (c) 2015年 leichunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
