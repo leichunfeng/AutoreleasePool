e49b59149d0d79476476d23840507960b43c70be
leichunfeng <307213080@qq.com>
2015-05-30 10:37:09 +0800

    Add experiment code.

--- AutoreleasePool/ViewController.m
+++ AutoreleasePool/ViewController.m
@@ -8,20 +8,41 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
+
+__weak NSString *string_weak_ = nil;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 第 1 种
+    NSString *string = [NSString stringWithFormat:@"leichunfeng"];
+    string_weak_ = string;
+    
+    // 第 2 种
+//    @autoreleasepool {
+//        NSString *string = [NSString stringWithFormat:@"leichunfeng"];
+//        string_weak_ = string;
+//    }
+    
+    // 第 3 种
+//    NSString *string = nil;
+//    @autoreleasepool {
+//        string = [NSString stringWithFormat:@"leichunfeng"];
+//        string_weak_ = string;
+//    }
+    
+    NSLog(@"string: %@", string_weak_);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    NSLog(@"string: %@", string_weak_);
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSLog(@"string: %@", string_weak_);
 }
 
 @end
